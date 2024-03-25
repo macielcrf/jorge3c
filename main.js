bff431cdd366e654a60ebba0874770a0c8eea325
macielcrf <128715902+macielcrf@users.noreply.github.com>
2024-03-25 09:08:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
+const botoes=document.queryselectorall(".botao");
+const textos=document.queryselectorall(".aba-conteudo");
+botoes[j].classlist.remove("ativo");
+textos[j].classlist.remove("ativo");
 
